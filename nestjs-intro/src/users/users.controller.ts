382b59e03109ae53fc450146597a9f413ef49607
mosh-d <shopsy8@gmail.com>
2026-03-20 18:40:36 +0100

    Adding validation class in NestJS Intro

--- nestjs-intro/src/users/users.controller.ts
+++ nestjs-intro/src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+  Headers,
+  Ip,
+  ParseIntPipe,
+  DefaultValuePipe
+} from '@nestjs/common';
+
+// http://localhost:3000/users
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/:id')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(limit);
+    console.log(page);
+    return 'You sent a get request to users endpoint';
+  }
+
+  @Post()
+  public createUsers(
+    @Body() request: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log(request);
+    console.log(headers);
+    console.log(ip);
+    return 'You sent a post request to users endpoint';
+  }
+}
